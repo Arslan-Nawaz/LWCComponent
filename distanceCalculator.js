2c72832e8929dba221446f14906afdfaf9eeeb1f
Arslan-Nawaz <135359611+Arslan-Nawaz@users.noreply.github.com>
2023-06-13 23:43:06 +0530

    Update distanceCalculator.js

--- distanceCalculator.js
+++ distanceCalculator.js
@@ -2,6 +2,7 @@ import { LightningElement } from 'lwc';
 import getAddress from '@salesforce/apex/SearchApiAddressCtrl.getAddress';
 import getAddressDetailsByPlaceId from '@salesforce/apex/SearchApiAddressCtrl.getPlaceDetails';
 import getCalculatedDistAndTime from '@salesforce/apex/SearchApiAddressCtrl.calculateDistAndTime';
+import getFareValueForModeOfTravel from '@salesforce/apex/SearchApiAddressCtrl.getFareValueForMode';
 export default class DistanceCalculator extends LightningElement {
     addressRecommendations = [];
     destinationAddressRecommendations = [];
@@ -9,12 +10,28 @@ export default class DistanceCalculator extends LightningElement {
     selectedOriginAddress='';
     selectedDestinationAddress='';
     addressDetail = {};
+    calculatedDistance;
+    calculatedTime;
     city;
     country;
     pincode;
     state;
     originPlaceId='';
     destinationPlaceId='';
+    // travelOptions = [
+    //     {'Label':'Bicycling', 'Value': 'Bicycling'},
+    //     {'Label': 'Walking', 'Value' : 'Walking'},
+    //     {'Label' : 'Driving', 'Value' : 'Driving'},
+    //     {'Label' : 'Transit', 'Value' : 'Transit'}
+    // ];
+    get travelOptions() {
+        return [
+            { label: 'Walking', value: 'walking' },
+            { label: 'Driving', value: 'driving' },
+            { label: 'Transit', value: 'transit' },
+        ];
+    }
+    selectedModeValue;
     get hasRecommendations() {
         return (this.addressRecommendations !== null && this.addressRecommendations.length);
     }
@@ -132,7 +149,7 @@ export default class DistanceCalculator extends LightningElement {
         if(this.selectedAddress!=null && this.selectedDestinationAddress!=null){
             console.log('The origin place id is:::::',this.originPlaceId);
             console.log('The destination place id is :::::',this.destinationPlaceId);
-            getCalculatedDistAndTime({ placeIdOfOrigin: this.originPlaceId, placeIdOfDestination: this.destinationPlaceId })
+            getCalculatedDistAndTime({ placeIdOfOrigin: this.originPlaceId, placeIdOfDestination: this.destinationPlaceId, modeOfTravel: this.selectedModeValue })
             .then(response => {
                 console.log('The response is received in the method:::::',response);
                 // response.result.address_components.forEach(address => {
@@ -148,5 +165,17 @@ export default class DistanceCalculator extends LightningElement {
                 console.log('error : ' + JSON.stringify(error));
             });
         }
+    }
+
+    selectedMode(event){
+        this.selectedModeValue = event.target.value;
+        getFareValueForModeOfTravel({ modeOfTravel : this.selectedModeValue})
+        .then(response =>{
+            console.log('The resonse received is:::::', response);
+        })
+        .catch(error => {
+            console.log('error : ' + JSON.stringify(error));
+        });
+
     }
 }
